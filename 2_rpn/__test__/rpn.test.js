d1f567a3ed265f254f21200662577d8b07fa02f3
assuy <43821373+assuy@users.noreply.github.com>
2023-04-11 19:32:01 +0200

    Add rpn

---
+++ 2_rpn/__test__/rpn.test.js
@@ -0,0 +1,7 @@
+const rpn = require("../rpn");
+
+describe("RPN", () => {
+  test("should convert string to rpn", () => {
+    expect(rpn("4 * ( 5 - 6 / 3 + 1 ) ^ 2")).toBe("4,5,6,3,/,-,1,+,2,^,*");
+  });
+});
