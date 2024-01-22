fcd993b6040e530d7acf192245c419085e166808
assuy <43821373+assuy@users.noreply.github.com>
2024-01-23 00:49:04 +0100

    Update header styling

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,13 +1,28 @@
 import Link from "next/link";
 
 const Header = () => {
-   return ( 
-      <div className="flex gap-2">
-         <Link href="/">Home</Link>
-         <Link href="/structures">Data Structures</Link>
-         <Link href="/algorithms">Algorithms</Link>
-      </div>
-   )
+   return (
+     <div className="w-fit m-2 px-4 py-2 shadow-lg rounded-md flex gap-5">
+       <Link
+         href="/"
+         className="hover:text-[#61398F] hover:scale-110 hover:font-semibold transition-all ease-in-out"
+       >
+         Home
+       </Link>
+       <Link
+         href="/structures"
+         className="hover:text-[#61398F] hover:scale-110 hover:font-semibold transition-all ease-in-out"
+       >
+         Data Structures
+       </Link>
+       <Link
+         href="/algorithms"
+         className="hover:text-[#61398F] hover:scale-110 hover:font-semibold transition-all ease-in-out"
+       >
+         Algorithms
+       </Link>
+     </div>
+   );
 };
 
 export default Header;
