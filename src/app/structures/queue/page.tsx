01a0af3ec657edf371d265f970fda8f6d348a0ab
assuy <43821373+assuy@users.noreply.github.com>
2024-01-31 22:33:31 +0100

    Add graph image and summary

--- src/app/structures/queue/page.tsx
+++ src/app/structures/queue/page.tsx
@@ -35,47 +35,60 @@ const Queue = () => {
   };
 
   return (
-    <div className="flex flex-col items-center mt-20">
-      <h1 className="text-4xl">Queue</h1>
-      <div className="my-5 px-4 py-2 shadow-lg rounded-md flex gap-4">
-        <input
-          type="text"
-          value={newItem}
-          onChange={handleInputChange}
-          onKeyUp={handleInputKeyPress}
-          placeholder="Enter value.."
-          className="w-32 px-4 py-2 bg-[#F5F3F7] shadow-inner rounded-md"
-        />
-        <button
-          onClick={enqueue}
-          className="hover:text-[#61398F] hover:font-semibold hover:scale-110 transition-all ease-in-out"
-        >
-          Enqueue
-        </button>
-        <button
-          onClick={dequeue}
-          className="hover:text-[#61398F] hover:font-semibold hover:scale-110 transition-all ease-in-out"
-        >
-          Dequeue
-        </button>
-        <button
-          onClick={clearQueue}
-          className="hover:text-[#878787] hover:font-semibold hover:scale-110 transition-all ease-in-out"
-        >
-          Clear
-        </button>
-      </div>
-      <div className="flex gap-3 flex-wrap items-center">
-        {queue.map((item, index) => (
-          <div
-            key={index}
-            className="w-16 h-16 bg-[#9A73B5] text-white rounded-md flex justify-center items-center"
-          >
-            {item}
-          </div>
-        ))}
-      </div>
-    </div>
+     <div className="flex flex-col items-center mt-20">
+        <h1 className="text-5xl mb-5">Queue</h1>
+
+        <div className="flex flex-col items-center mx-10">
+           <img src="/queue.png" alt="queue" className="max-w-1/3" />
+           <p className="my-5 text-justify w-fit md:w-2/3 lg:w-1/3">
+              Queue is a linear data structure that follows a particular order
+              in which the operations are performed for storing data. The order
+              it <b>First In First Out (FIFO)</b>. One can imagine a queue as a
+              line of people waiting to receive something in sequential order
+              which starts from the beginning of the line.
+           </p>
+        </div>
+
+        <h2 className="text-2xl">Demonstration</h2>
+        <div className="my-5 px-4 py-2 shadow-lg rounded-md flex gap-4">
+           <input
+              type="text"
+              value={newItem}
+              onChange={handleInputChange}
+              onKeyUp={handleInputKeyPress}
+              placeholder="Enter value.."
+              className="w-32 px-4 py-2 bg-[#F5F3F7] shadow-inner rounded-md"
+           />
+           <button
+              onClick={enqueue}
+              className="hover:text-[#61398F] hover:font-semibold hover:scale-110 transition-all ease-in-out"
+           >
+              Enqueue
+           </button>
+           <button
+              onClick={dequeue}
+              className="hover:text-[#61398F] hover:font-semibold hover:scale-110 transition-all ease-in-out"
+           >
+              Dequeue
+           </button>
+           <button
+              onClick={clearQueue}
+              className="hover:text-[#878787] hover:font-semibold hover:scale-110 transition-all ease-in-out"
+           >
+              Clear
+           </button>
+        </div>
+        <div className="flex gap-3 flex-wrap items-center">
+           {queue.map((item, index) => (
+              <div
+                 key={index}
+                 className="w-16 h-16 bg-[#9A73B5] text-white rounded-md flex justify-center items-center"
+              >
+                 {item}
+              </div>
+           ))}
+        </div>
+     </div>
   );
 };
 
